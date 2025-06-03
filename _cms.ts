6780b08c012cea9d75a2bc7658746356d420066f
Lex Feathers <lex@gameaudio.ca>
2025-06-03 19:40:34 -0400

    Removing basePath

--- _cms.ts
+++ _cms.ts
@@ -17,7 +17,6 @@ const formattedDate = humanDate.toISOString().split('T')[0].replace(/-/g, '-');
 
 // Create the cms instance
 const cms = lumeCMS({
-  basePath: "/admin",
   site: {
     name: "Lex Feathers",
     url: "https://lexfeathers.ca",
