914d734dea7f0d9294e4cd7191b7af0f860d504d
Lex Feathers <lex@gameaudio.ca>
2025-06-05 17:05:47 -0400

    Added alert plugin, changed order of Google fonts plugin usage, removed lastmod option from sitemap plugin

--- _config.ts
+++ _config.ts
@@ -10,6 +10,8 @@ import footnote from "npm:markdown-it-footnote";
 import implicitFigures from "npm:markdown-it-image-figures";
 import extractDate from "lume/plugins/extract_date.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
+import inline from "lume/plugins/inline.ts";
+import icons from "lume/plugins/icons.ts";
 // import picture from "lume/plugins/picture.ts";
 // import transformImages from "lume/plugins/transform_images.ts";
 import googleFonts from "lume/plugins/google_fonts.ts";
@@ -19,7 +21,9 @@ import { alert } from "npm:@mdit/plugin-alert@0.8.0";
 const markdown = {
   plugins: [
     footnote,
-    alert,
+    [alert, {
+      deep: true,
+    }],
     [implicitFigures, {
       lazy: true,
       async: true,
@@ -48,6 +52,14 @@ site.preprocess([".html"], (pages) => {
   }
 });
 
+site.use(googleFonts({
+  fonts:
+    "https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap",
+}));
+site.use(inline());
+site.use(icons({
+  folder: "/assets/images/icons"
+}));
 site.use(extractDate());
 site.use(
   codeHighlight ({
@@ -58,17 +70,13 @@ site.use(
     },
   }),
 );
-site.use(googleFonts({
-  fonts:
-    "https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap",
-}));
 site.use(favicon({
   input: "/assets/favicon.ico",
 }));
 site.use(sitemap({
   filename: "sitemap.xml", // to change the sitemap filename
   sort: "published=desc", // To sort by data in ascendent order
-  lastmod: "=lastmod",
+  // lastmod: "=lastmod",
   stylesheet: "/assets/linked.xslt"
 }));
 site.use(date({
