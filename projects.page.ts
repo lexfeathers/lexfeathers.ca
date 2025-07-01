523f7518a64cfbc1032dfb5cb19d0525c8613994
Lex Feathers <lex@gameaudio.ca>
2025-06-30 22:10:17 -0400

    Working on adding pagination

---
+++ projects.page.ts
@@ -0,0 +1,11 @@
+export default function* ({ search, paginate }) {
+  const posts = search.pages("type=projects", "published=desc");
+  const options = {
+    url: (n) => `/portfolio/page-${n}/`,
+    size: 12,
+  };
+
+  for (const page of paginate(posts, options)) {
+    yield page;
+  }
+}
