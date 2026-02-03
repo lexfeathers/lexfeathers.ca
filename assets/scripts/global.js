368622c065e4708cd06db88b790c4019fc092ed1
Lex <38232476+lexfeathers@users.noreply.github.com>
2026-02-03 10:45:09 -0500

    Added document readyState check

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,4 +1,12 @@
-// Make all external links open in a new tab and apply external class
-Array.from(document.links)
+// Run script when document loads
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", getLinks);
+} else {
+    getLinks();
+}
+function getLinks() {
+    // Make all external links open in a new tab and apply external class
+    Array.from(document.links)
     .filter(link => link.hostname != window.location.hostname)
     .forEach(link => link.target = '_blank' && link.classList.add('external'));
+}
