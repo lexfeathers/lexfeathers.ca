812c449450959d86406d2044d268daa84ddce9db
Lex <38232476+lexfeathers@users.noreply.github.com>
2026-02-03 11:06:57 -0500

    Change window to globalThis

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -7,6 +7,6 @@ if (document.readyState === "loading") {
 function getLinks() {
     // Make all external links open in a new tab and apply external class
     Array.from(document.links)
-    .filter(link => link.hostname != window.location.hostname)
+    .filter(link => link.hostname != globalThis.location.hostname)
     .forEach(link => link.target = '_blank' && link.classList.add('external'));
 }
