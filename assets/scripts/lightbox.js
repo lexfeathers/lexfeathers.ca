16fde3572965a737785c1c923e637d36e93f895e
Lex <38232476+lexfeathers@users.noreply.github.com>
2026-02-06 14:30:52 -0500

    Blur backdrop and allow for escape key lightbox exit

--- assets/scripts/lightbox.js
+++ assets/scripts/lightbox.js
@@ -12,9 +12,20 @@ function getImages() {
   images.forEach((image, index) => {
     image.id = `image-lightbox-${index}`;
     image.addEventListener("click", lightbox);
-  });
+
+});
 }
 
 function lightbox() {
-  this.classList.toggle("lightbox");
+    this.classList.toggle("lightbox");
 }
+
+// Toggle lightbox class off when using escape key
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        // IKOV: in = keys, of = values
+        for (const element of document.getElementsByClassName("lightbox")) {
+            element.classList.remove("lightbox");
+        }
+    }
+});
